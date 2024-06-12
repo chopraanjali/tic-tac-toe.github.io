19b3a3282b17b8b91c02d79fbac8fc52f030924c
anjali <anjalichopra1994@gmail.com>
2024-06-12 19:59:50 +0530

    fix?vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tic-tac-toe.github.io',
+  base: './tic-tac-toe.github.io',
   plugins: [react()],
 });
